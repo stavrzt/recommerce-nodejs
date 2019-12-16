2b2795a2518c87a073634cc519a9976be7d43f7e
y.mudryi <y.mudryi@astoundcommerce.com>
2019-12-16 12:18:02 +0200

    Changed port and removed excess routes

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
+// view engine setup
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// set static dir
 app.use(express.static(path.join(__dirname, 'public')));
 
+// routes
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 module.exports = app;
